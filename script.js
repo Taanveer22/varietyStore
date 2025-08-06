1821fde62c4d793569c27884a7db3e89ef01d407
Taanveer22 <taanveer469@gmail.com>
2025-08-06 15:57:06 +0600

    alhamdulliah data added to cart and project done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("connected");
 // function 01=================================================
 const loadAllProducts = async (query = "") => {
   document.getElementById("loading-container").style.display = "block";
-  console.log(query);
+  // console.log(query);
   const res = await fetch(`https://fakestoreapi.com/products${query}`);
   const data = await res.json();
   // console.log(data);
@@ -25,28 +25,29 @@ const loadAllProducts = async (query = "") => {
     div.className = "product";
     div.innerHTML = `
     <img
-      class="product-img"
-      src="${product.image}"
-      alt=""
+      class = "product-img"
+      src = "${product.image}"
     />
     <h3>${product.title.slice(0, 15)}</h3>
-    <div class="price-category">
-      <h4>price: ${product.price}</h4>
-      <button class="category-btn" disabled>category: ${
+    <div class = "price-category">
+      <h4>price : ${product.price}</h4>
+      <button class = "category-btn" disabled>category: ${
         product.category
       }</button>
     </div>
     <hr />
-    <div class="price-container">
-      <div class="icon-container">
-        <img class="icon" src="./image/view.png" alt="" />
+    <div class = "price-container">
+      <div class = "icon-container">
+        <img class = "icon" src="./image/view.png" alt="" />
         <h4>${product.rating.count}</h4>
       </div>
-      <div class="icon-container">
-        <img class="icon" src="./image/star.png" alt="" />
+      <div class = "icon-container">
+        <img class = "icon" src="./image/star.png" alt="" />
         <h4>${product.rating.rate}</h4>
       </div>
-      <button class="add-btn">Add To Cart</button>
+      <button onclick= "addToCart('${product.title.slice(0, 15)}', '${
+      product.price
+    }')" class="add-btn">Add To Cart</button>
     </div>
 
     `;
@@ -59,6 +60,19 @@ const handleSearch = () => {
   const value = document.getElementById("search-box").value;
   // console.log(value);
   loadAllProducts(`/category/${value}`);
+};
+
+// function 03===============================================
+const addToCart = (name, price) => {
+  console.log(name, price);
+  const cartData = document.getElementById("cart-data");
+  const div = document.createElement("div");
+  div.classList.add("cart-data-sort");
+  div.innerHTML = `
+  <p>${name}</p>
+  <p>${price}</p>
+  `;
+  cartData.appendChild(div);
 };
 
 // function invocation
